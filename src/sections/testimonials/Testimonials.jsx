4c6dfb60b71b42f5dca634e45e9a6ba0f53809e8
aleksi-svaridze <aleksi.svaridze82@gmail.com>
2025-12-04 04:56:10 +0400

    Add Testimonials data from google

--- src/sections/testimonials/Testimonials.jsx
+++ src/sections/testimonials/Testimonials.jsx
@@ -1,5 +1,18 @@
 import Styles from "./Testimonials.module.scss";
+import DATA from "../../DATA/testimonials_array.json";
 
 export const Testimonials = () => {
-  return <div className={`main-container`}>Testimonials</div>;
+  console.log(DATA);
+  return (
+    <div className={`${Styles.container} bg-white`}>
+      <div className="main-container bg-red-500">
+        {DATA.map((user) => (
+          <div key={user.id}>
+            <h3>{user.person}</h3>
+            <p>{user.review}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
